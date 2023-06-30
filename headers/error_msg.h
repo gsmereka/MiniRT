c1047b15a830c3a103f1a89b8be8952500769bf4
gsmereka <gabrielsmereka200@gmail.com>
2023-06-29 22:21:31 -0300

    resolve conflits

--- headers/error_msg.h
+++ headers/error_msg.h
@@ -6,13 +6,13 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 16:01:29 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/06/29 22:16:09 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/06/29 22:19:53 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_MSG_H
 # define ERROR_MSG_H
-# define OPEN_FILE_ERROR 10
+# define OPEN_FILE_ERROR "Error\nFile can not open\n"
 # define COUNT_ERROR "Error\nOnly one file allowed\n"
 # define FILE_EXTENSION_ERROR "Error\nOnly files with the '.rt' \
 extension allowed\n"
