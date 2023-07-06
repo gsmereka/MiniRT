86737f7ea0c7f8adc2f3520e39ece08de47619e8
gsmereka <gabrielsmereka200@gmail.com>
2023-07-06 19:49:36 -0300

    Att tester

--- test/test.sh
+++ test/test.sh
@@ -15,6 +15,11 @@ EXPECTED_FOLDER="./expected"
 PROGRAM="../miniRT"
 
 
+# Contagem do numero de testes e de testes passados
+TEST_COUNT=0
+PASSED_TESTS_COUNT=0
+LEAKS=0
+
 
 # Mensagem utilizada pra verificar se não houve vazamentos.
 FREE_LEAKS_IDENTIFY_MSG="All heap blocks were freed -- no leaks are possible"
@@ -30,6 +35,8 @@ GREEN_COLOR="\033[0;32m"
 RED_COLOR="\033[0;31m"
 
 WHITE_COLOR="\033[0m"
+
+YELLOW_COLOR="\033[0;33m"
 
 # Função para criar uma pasta se ela não existir.
 create_folder_if_not_exists() {
@@ -69,6 +76,8 @@ for file in "$TESTS_FOLDER"/*; do
   if [ -f "$file" ]; then
     echo "Executing miniRT with the file: $file"
 
+    TEST_COUNT=$((TEST_COUNT + 1))
+  
     # Executa o programa normalmente e salva os resultados na pasta RESULTS_FOLDER.
     "timeout" "$TIME_LIMIT" "$PROGRAM" "$file" 2> "$RESULTS_FOLDER/$(basename "$file")" >/dev/null
 
@@ -76,6 +85,9 @@ for file in "$TESTS_FOLDER"/*; do
     # Compara o conteudo salvo no arquivo em RESULTS_FOLDER, com seu respectivo arquivo na pasta EXPECTED_FOLDER.
     if diff "$RESULTS_FOLDER/$(basename "$file")" "$EXPECTED_FOLDER/$(basename "$file")" >/dev/null ; then
       echo -e  "EXPECTED RESULT: " "$GREEN_COLOR" "OK" "$WHITE_COLOR"
+
+      PASSED_TESTS_COUNT=$((PASSED_TESTS_COUNT + 1))
+
     else
       echo -e  "EXPECTED RESULT: " "$RED_COLOR" "KO" "$WHITE_COLOR"
     fi
@@ -90,9 +102,35 @@ for file in "$TESTS_FOLDER"/*; do
       echo -e "$GREEN_COLOR" "NO LEAKS" "$WHITE_COLOR"
     else
       echo -e "$RED_COLOR" "LEAKS" "$WHITE_COLOR"
-
+      LEAKS=$((LEAKS + 1))
     fi
-  
-    echo
   fi
+  echo
 done
+
+# Mensagens
+TESTS_COUNT_MSG="Resultado dos Testes: $PASSED_TESTS_COUNT/$TEST_COUNT" 
+LEAKS_COUNT_MSG="Testes com Vazamentos: $LEAKS/$TEST_COUNT"
+
+OK_MSG="Tudo Perfeito e sem Vazamentos :D"
+STILL_MSG="Quase lá :D"
+KO_MSG="Ainda não T-T"
+
+# Verifica se todos os testes foram aprovados e não houve vazamentos
+if [ "$PASSED_TESTS_COUNT" -eq "$TEST_COUNT" ] && [ "$LEAKS" -eq "0" ] ; then
+  echo -e "$GREEN_COLOR" "$TESTS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$GREEN_COLOR" "$LEAKS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$GREEN_COLOR" "$OK_MSG" "$WHITE_COLOR"
+elif [ "$PASSED_TESTS_COUNT" -eq "$TEST_COUNT" ] ; then
+  echo -e "$YELLOW_COLOR" "$TESTS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$YELLOW_COLOR" "$LEAKS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$YELLOW_COLOR" "$STILL_MSG" "$WHITE_COLOR"
+elif [ "$LEAKS" -eq "0" ] ; then
+  echo -e "$YELLOW_COLOR" "$TESTS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$YELLOW_COLOR" "$LEAKS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$YELLOW_COLOR" "Pelo menos não tem vazamentos..." "$WHITE_COLOR"
+else
+  echo -e "$RED_COLOR" "$TESTS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$RED_COLOR" "$LEAKS_COUNT_MSG" "$WHITE_COLOR"
+  echo -e "$RED_COLOR" "$KO_MSG" "$WHITE_COLOR"
+fi
