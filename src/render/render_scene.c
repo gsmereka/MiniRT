3601ea3f5711d8d6daa0b737a98a07349c75e22b
gsmereka <gsmereka200@gmail.com>
2023-11-19 13:21:50 -0300

    real time render

--- src/render/render_scene.c
+++ src/render/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 23:04:34 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/11/04 16:26:50 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/11/19 13:20:12 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	render_scene(t_scene *scene, t_camera *camera, t_data *data)
 					scene->object_ray, scene->light_ray);
 			pixel_color = color_to_int(&ray_color);
 			paint_pixel(j, i, pixel_color, data);
+			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img, 0, 0);
 			j++;
 		}
 		i++;
